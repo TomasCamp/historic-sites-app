18b3ba0020dfc72336f3dc6a4d7bb279a298119f
Tomás Campanella <tomyecamp@gmail.com>
2026-07-02 11:46:40 -0300

    fix: agregar server_default en atributo de change_event

--- src/models/change_event/change_event.py
+++ src/models/change_event/change_event.py
@@ -2,7 +2,7 @@ from src import db
 from datetime import datetime
 from typing import TYPE_CHECKING
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from sqlalchemy import DateTime, ForeignKey, String
+from sqlalchemy import DateTime, ForeignKey, String, func
 
 
 if TYPE_CHECKING:
@@ -14,7 +14,9 @@ class ChangeEvent(db.Model):
     __tablename__ = "change_events"
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    timestamp: Mapped[datetime] = mapped_column(DateTime, nullable=False)
+    timestamp: Mapped[datetime] = mapped_column(
+        DateTime, nullable=False, server_default=func.now()
+    )
     action: Mapped[str] = mapped_column(String(15), nullable=False)
 
     user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
