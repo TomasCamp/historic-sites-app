0b5d18f2de4b473f778636b82e8dfdc988d91588
Tomás Campanella <tomyecamp@gmail.com>
2026-07-09 19:36:10 -0300

    feat: agregar función crear user

--- src/controllers/user_controller.py
+++ src/controllers/user_controller.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask_login import login_required
 from src.controllers.decorators import permission_required
 from src.services import user_service
@@ -21,3 +21,27 @@ def index():
     users = user_service.list_filtered_users(**filters)
 
     return render_template("users/index.html", users=users, form=form)
+
+
+@bp.route("/create", methods=["GET", "POST"])
+@login_required
+@permission_required("user_create")
+def create():
+    """Muestra el formulario y procesa la creación de un nuevo usuario."""
+    form = user_forms.UserCreateForm()
+
+    if form.validate_on_submit():
+        user_data = form.data.copy()
+        user_data.pop("password_confirm", None)
+        user_data.pop("csrf_token", None)
+        new_user = user_service.create_user(**user_data)
+        if new_user:
+            flash("Usuario creado correctamente.", "success")
+            return redirect(url_for("users.index"))
+        flash("El email ingresado ya pertenece a un usuario registrado.", "danger")
+    else:
+        for field_name, error_messages in form.errors.items():
+            for error in error_messages:
+                flash(error, "danger")
+
+    return render_template("users/create.html", form=form)
--- src/forms/user_forms.py
+++ src/forms/user_forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SelectField
+from wtforms import StringField, SelectField, EmailField, PasswordField
 from src.services import user_service
+from wtforms.validators import DataRequired, Length, Email, Regexp, EqualTo
 
 
 class UserFilterForm(FlaskForm):
@@ -20,10 +21,65 @@ class UserFilterForm(FlaskForm):
 
     def __init__(self, *args, **kwargs):
         kwargs["meta"] = {"csrf": False}
-        super(UserFilterForm, self).__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)
 
         roles = user_service.list_all_roles()
 
         self.role_id.choices = [("", "Todos")] + [
+            (str(role.id), role.name) for role in roles
+        ]
+
+
+class UserCreateForm(FlaskForm):
+    name = StringField(
+        "Nombre",
+        validators=[
+            DataRequired("El campo nombre es obligatorio."),
+            Length(3, 60, "El campo nombre debe tener entre 3 y 60 caracteres."),
+        ],
+    )
+    lastname = StringField(
+        "Apellido",
+        validators=[
+            DataRequired("El campo apellido es obligatorio."),
+            Length(3, 60, "El campo apellido debe tener entre 3 y 60 caracteres."),
+        ],
+    )
+    email = EmailField(
+        "Email",
+        validators=[
+            DataRequired("El campo email es obligatorio."),
+            Length(3, 120, "El campo email debe tener entre 3 y 120 caracteres."),
+            Email("El formato del campo email debe ser válido."),
+        ],
+    )
+    role_id = SelectField(
+        "Rol", validators=[DataRequired("Debe seleccionar un rol para el usuario.")]
+    )
+    password = PasswordField(
+        "Contraseña",
+        validators=[
+            DataRequired("El campo contraseña es obligatorio."),
+            Length(6, 64, "El campo contraseña debe tener entre 6 y 64 caracteres."),
+            Regexp(
+                r"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d).*$",
+                message="El campo contraseña debe tener al menos una mayúscula, una minúscula y un número.",
+            ),
+        ],
+    )
+    password_confirm = PasswordField(
+        "Repetir Contraseña",
+        validators=[
+            DataRequired("El campo repetir contraseña es obligatorio."),
+            EqualTo("password", "Las contraseñas no coinciden."),
+        ],
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        roles = user_service.list_all_roles()
+
+        self.role_id.choices = [("", "Seleccione un rol...")] + [
             (str(role.id), role.name) for role in roles
         ]
